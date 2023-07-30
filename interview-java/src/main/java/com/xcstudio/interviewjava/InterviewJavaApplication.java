805538c1317dfb469286ef7580ee9cf6c9c0bf67
wenxiaocheng <634650981@qq.com>
2023-07-30 09:42:13 +0800

    init interview-java

---
+++ interview-java/src/main/java/com/xcstudio/interviewjava/InterviewJavaApplication.java
@@ -0,0 +1,13 @@
+package com.xcstudio.interviewjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewJavaApplication.class, args);
+    }
+
+}
---
+++ interview-java/src/test/java/com/xcstudio/interviewjava/InterviewJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xcstudio.interviewjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
